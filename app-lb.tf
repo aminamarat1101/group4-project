a0eb772907fce0c05c48a94700d516e12ec2a4b1
aminamarat1101 <aminamaratt@gmail.com>
2025-02-22 03:05:21 +0000

    added

--- app-lb.tf
+++ app-lb.tf
@@ -17,7 +17,7 @@ resource "aws_lb_listener" "http" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.blue.arn
+    target_group_arn = aws_lb_target_group.green.arn
   }
 }
 
